d8337c45162d9fca2a3005e96a94d8d5fa7d64c5
raneem moualla <raneemmoualla@gmail.com>
2021-10-09 12:38:12 +0200

    add backend

---
+++ Backend/models/comment.js
@@ -0,0 +1,17 @@
+'use strict';
+const { Model } = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Comment extends Model {};
+  //initialize a model, representing a table in the DB, with attributes and options.
+  Comment.init({
+    comment_user: DataTypes.INTEGER,
+    comment_message: DataTypes.STRING,
+    comment_content: DataTypes.STRING,
+    comment_image: DataTypes.STRING,
+  }, {
+    sequelize,
+    modelName: 'Comment',
+    timestamps: false
+  });
+  return Comment;
+}; 
---
+++ Backend/models/index.js
@@ -0,0 +1,41 @@
+'use strict';
+
+const fs = require('fs');
+const path = require('path');
+const Sequelize = require('sequelize');
+const basename = path.basename(__filename);
+require('dotenv').config({ path: process.cwd() + '/.env' });
+const db = {};
+// Configuration Environment
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
+    host: process.env.DB_HOST, dialect: 'mysql', port: process.env.DB_PORT })
+// Importing/Reading Models
+//Filter function is just making sure that:
+//There are files to read
+//It excludes this file (iS not index.js)
+//They end in '.js'
+fs
+  .readdirSync(__dirname)
+  .filter(file => {
+    return (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js');
+  })
+  .forEach(file => {
+    const model = require(path.join(__dirname, file))(sequelize, Sequelize.DataTypes);
+    db[model.name] = model;
+  });
+  //Associating Each Model
+
+Object.keys(db).forEach(modelName => {
+  if (db[modelName].associate) {
+    db[modelName].associate(db);
+  }
+});
+//Both an instance of OUR sequelize database (sequelize) and an instance of the Sequelize library are exported.
+db.sequelize = sequelize;
+db.Sequelize = Sequelize;
+
+db.users = require('./user.js')(sequelize, Sequelize);
+db.messages = require('./message.js')(sequelize, Sequelize);
+db.comments = require('./comment.js')(sequelize, Sequelize);
+
+module.exports = db; 
